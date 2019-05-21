2b0192dd1f20d16ca8e278ab15368853af8e6c8b
navyakovuru@gmail.com <navyakovuru@gmail.com>
2019-05-21 14:39:14 +0530

    modified content of compile.bat file - added append mode to log files

--- source/run.bat
+++ source/run.bat
@@ -3,4 +3,4 @@ cd D:\NAVYA\DevOps\Interns2019\source\debug
 cd debug 
 realCalculatorString.exe 
 cd.. 
-echo Execution Successfully Completed>runLOG.log
+echo Execution Successfully Completed>>runLOG.log
